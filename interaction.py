9b511639b443bc9e682c6e833505e78eba317d17
Timbo <5blaesche@informatik.uni-hamburg.de>
2018-06-22 14:24:12 +0200

    Implements the email sending as well as picture taking

--- interaction.py
+++ interaction.py
@@ -1,10 +1,18 @@
+import os
 import time
 import subprocess
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+from_address = "timbo@tributi.de"
 
 
 def start_conversation():
     print("\033[H\033[J")
-    print("Welcome to the Christoperus Photobooth! We are happy you are here!")
+    print("Welcome to the Christophorus Photobooth! We are happy you are here!")
 
     response = input("Do you want to take a picture? [yes, y, no, n]\n")
     return response
@@ -22,6 +30,46 @@ def countdown(n):
 
 
 def take_picture():
-    print("SMILE! :D")
-    subprocess.call("image.sh")
-    print("Your image was captured!")
+    print("SMILE! :D\n")
+    subprocess.call("./image.sh")
+    print("Your image was captured!\n")
+
+
+def get_email_addresses():
+    print("Please enter your email to receive the image.")
+    addresses = input("More than one address is possible. Please separate them with ';'\n")
+    return addresses
+
+
+def send_mail_to(address):
+    print("Preparing image to be sent")
+    msg = MIMEMultipart()
+    msg["From"] = from_address
+    msg["To"] = address
+    msg["Subject"] = "CH-Photobooth"
+
+    body = "In the attachment you will find your personal image!\nThis is a unique picture it was deleted" \
+           " from the photobooth for privacy reasons.\n\nWe hope you had a great time!" \
+           "\nChristophorus Haus Semesterparty Team"
+    msg.attach(MIMEText(body, "plain"))
+    filename = "~/Documents/capture.jpg"
+    attachment = open(filename, "rb")
+
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    print("Encoding image")
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+    msg.attach(part)
+
+    print("Establishing connection")
+    server = smtplib.SMTP("mailout.tributi.de", 587)
+    server.starttls()
+    print("Logging in to Mail Server")
+    server.login(os.environ["MAIL_USER"], os.environ["MAIL_PASSWORD"])
+    text = msg.as_string()
+    print("Sending mail...")
+    server.sendmail(from_address, address, text)
+    server.quit()
+    print("Email was sent to %s" % address)
--- photobooth.py
+++ photobooth.py
@@ -5,7 +5,9 @@ response = interaction.start_conversation()
 while response != "q":
     if response == "y" or response == "yes":
         interaction.countdown(5)
-        response = interaction.take_picture()
+        interaction.take_picture()
+        response = interaction.get_email_addresses()
+        interaction.send_mail_to(response)
     else:
         print("Sad to see you leave! Enjoy the party ;)")
         response = interaction.start_conversation()
